34d1b9f8bf262834a91eb006f2465fcb149b6955
Michael Feth <michael@jira-flow.com>
2026-05-04 15:32:18 -0400

    fix: normalize railway nats launcher

--- infra/railway-nats/start-nats.sh
+++ infra/railway-nats/start-nats.sh
@@ -2,9 +2,8 @@
 set -e
 NATS_PORT="${RAILWAY_TCP_APPLICATION_PORT:-4222}"
 
-# Unset PORT so NATS doesn't try to use it for the client port
+# Unset PORT so NATS doesn't try to use it for the client port.
 unset PORT
 
 echo "Starting NATS with port=$NATS_PORT, monitor=8222, jetstream=/data/jetstream"
 exec nats-server -p "$NATS_PORT" -m 8222 -js -sd /data/jetstream
-
